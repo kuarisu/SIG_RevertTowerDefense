ee0a09b4b5553cf7fbca1ef22eb9ae521a12fbc1
kuarisu <kuarisuran@gmail.com>
2017-03-30 14:39:04 +0200

    Waypoints and patrlos start

---
+++ Assets/Script/Car/Car_Driving.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Car_Driving : StateMachineBehaviour {
+
+    List<Transform> m_ListOfKeyPoints = new List<Transform>();
+
+    Transform m_TargetPosition;
+    NavMeshAgent m_Agent;
+    int m_indexOfNextTarget = 0;
+    int m_currentIndexOfNextTarget = 0;
+    bool m_HasArrivedDestination;
+    float m_DistanceToDestination;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        m_currentIndexOfNextTarget = m_indexOfNextTarget;
+        SetTarget(m_ListOfKeyPoints[m_currentIndexOfNextTarget].transform);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        m_Agent.destination = m_TargetPosition.position;
+        m_DistanceToDestination = Vector3.Distance(m_TargetPosition.position, m_Agent.transform.position);
+
+        if (m_DistanceToDestination < 3)
+        {
+
+            if (m_indexOfNextTarget < (m_ListOfKeyPoints.Count - 1))
+            {
+                SetTarget(m_ListOfKeyPoints[m_indexOfNextTarget].transform);
+                m_indexOfNextTarget++;
+            }
+            else if (m_indexOfNextTarget == m_ListOfKeyPoints.Count - 1)
+            {
+                SetTarget(m_ListOfKeyPoints[m_indexOfNextTarget].transform);
+                m_indexOfNextTarget = 0;
+            }
+        }
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+
+    public void SetKeyPointsPos(List<Transform> _ListOfKeyPosition)
+    {
+        m_ListOfKeyPoints = _ListOfKeyPosition;
+    }
+    public void SetTarget(Transform _newPos)
+    {
+        m_TargetPosition = _newPos;
+    }
+
+    public void SetAgent(NavMeshAgent _currentAgent)
+    {
+        m_Agent = _currentAgent;
+    }
+}
---
+++ Assets/Script/Manager/Manager_PatrolList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager_PatrolList : MonoBehaviour {
+
+    public static Manager_PatrolList Instance;
+
+    public List<Transform> m_ListOfWaypoints = new List<Transform>();
+
+    private void Awake()
+    {
+        if (Manager_PatrolList.Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Manager_PatrolList.Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
